07538b382156b73cbb23008b45e1db0e57752391
madwareru <madware.ru@gmail.com>
2021-07-25 11:11:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# simple-tiled-wfc
---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use bitsetium::BitSearch;
+
+pub mod errors;
+pub mod grid_generation;
+pub mod voxel_generation;
+
+pub(crate) struct BitsIterator<'a, T: BitSearch>  {
+    iterated: &'a T,
+    idx: usize
+}
+
+impl<'a, T: BitSearch> BitsIterator<'a, T> {
+    pub(crate) fn new(iterated: &'a T) -> Self {
+        Self {
+            iterated,
+            idx: 0
+        }
+    }
+}
+
+impl<'a, T: BitSearch> Iterator for BitsIterator<'a, T> {
+    type Item = usize;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        match self.iterated.find_first_set(self.idx) {
+            None => None,
+            Some(id) => {
+                self.idx = id + 1;
+                Some(id)
+            }
+        }
+    }
+}
+
+pub(crate) fn get_bits_set_count<'a, T: BitSearch>(bit_set: &T) -> usize {
+    BitsIterator::new(bit_set).fold(0, |acc, _| acc + 1)
+}
